189314f883812f9ab123af05a5967fe6bf63ee7d
kingpasu <68714638+kingpasu@users.noreply.github.com>
2020-08-11 12:01:25 -0400

    Add files via upload

---
+++ website.js
@@ -0,0 +1,30 @@
+function top1(){
+	var t1butt=document.getElementById("Topbutt1");
+	t1butt.style.display ="none";
+	document.getElementById("top1").style.display = "block";
+}
+function top2(){
+	var t2butt=document.getElementById("Topbutt2");
+	t2butt.style.display ="none";
+	document.getElementById("top2").style.display = "block";
+}
+function top3(){
+	var t3butt=document.getElementById("Topbutt3");
+	t3butt.style.display ="none";
+	document.getElementById("top3").style.display = "block";
+}
+function top4(){
+	var t4butt=document.getElementById("Topbutt4");
+	t4butt.style.display ="none";
+	document.getElementById("top4").style.display = "block";
+}
+function top5(){
+	var t5butt=document.getElementById("Topbutt5");
+	t5butt.style.display ="none";
+	document.getElementById("top5").style.display = "block";
+}
+function quest(){
+	var qbutt=document.getElementById("questbutt");
+	qbutt.style.display ="none";
+	document.getElementById("noform").style.display = "block";
+}
